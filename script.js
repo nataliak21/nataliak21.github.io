bfe61f8964d6397a3836811fe6e75fac2522989a
nataliak21 <nat8oct@gmail.com>
2025-07-24 14:27:41 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,39 +1,34 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const navLinks = document.querySelectorAll("#navbar-right a");
-  const toggle = document.getElementById("navbar-toggle");
-  const navbarRight = document.getElementById("navbar-right");
-
-  toggle.addEventListener("click", () => {
-    navbarRight.classList.toggle("active");
-  });
-
-  navLinks.forEach(link => {
-    link.addEventListener("click", () => {
-      navLinks.forEach(l => l.classList.remove("active"));
-      link.classList.add("active");
-      navbarRight.classList.remove("active"); // Zamknij menu po kliknięciu
-    });
-  });
-
-  function updateActiveNavLink() {
-    const sections = document.querySelectorAll("section");
-    let currentSectionId = "";
-
-    sections.forEach(section => {
-      const offsetTop = section.offsetTop - 200;
-      if (window.scrollY >= offsetTop) {
-        currentSectionId = section.getAttribute("id");
-      }
-    });
+window.addEventListener("DOMContentLoaded", () => {
+    const navLinks = document.querySelectorAll("#navbar-right a");
+  
 
     navLinks.forEach(link => {
-      link.classList.remove("active");
-      if (link.getAttribute("href") === `#${currentSectionId}`) {
+      link.addEventListener("click", () => {
+        navLinks.forEach(l => l.classList.remove("active"));
         link.classList.add("active");
-      }
+      });
     });
-  }
-
-  window.addEventListener("scroll", updateActiveNavLink);
-  updateActiveNavLink();
-});
+  
+    function updateActiveNavLink() {
+      const sections = document.querySelectorAll("section");
+      let currentSectionId = "";
+  
+      sections.forEach(section => {
+        const offsetTop = section.offsetTop - 200; 
+        if (window.scrollY >= offsetTop) {
+          currentSectionId = section.getAttribute("id");
+        }
+      });
+  
+      navLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === `#${currentSectionId}`) {
+          link.classList.add("active");
+        }
+      });
+    }
+    window.addEventListener("scroll", updateActiveNavLink);
+  
+    updateActiveNavLink();
+  });
+  
