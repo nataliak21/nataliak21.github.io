be6133c9cbb7fbd090fdba7fbf83483abf5a2b3c
nataliak21 <nat8oct@gmail.com>
2025-07-24 14:07:39 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -31,9 +31,4 @@ window.addEventListener("DOMContentLoaded", () => {
   
     updateActiveNavLink();
   });
-  const toggle = document.getElementById('navbar-toggle');
-    const menu = document.getElementById('navbar-right');
   
-    toggle.addEventListener('click', () => {
-      menu.classList.toggle('active');
-    });
