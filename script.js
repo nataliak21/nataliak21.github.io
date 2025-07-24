982c19ce867ad95f1d1868ddd8e40cd64f4b870a
szwabix21 <nat8oct@gmail.com>
2025-07-24 13:13:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const navLinks = document.querySelectorAll("#navbar-right a");
+  
+
+    navLinks.forEach(link => {
+      link.addEventListener("click", () => {
+        navLinks.forEach(l => l.classList.remove("active"));
+        link.classList.add("active");
+      });
+    });
+  
+    function updateActiveNavLink() {
+      const sections = document.querySelectorAll("section");
+      let currentSectionId = "";
+  
+      sections.forEach(section => {
+        const offsetTop = section.offsetTop - 200; 
+        if (window.scrollY >= offsetTop) {
+          currentSectionId = section.getAttribute("id");
+        }
+      });
+  
+      navLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === `#${currentSectionId}`) {
+          link.classList.add("active");
+        }
+      });
+    }
+  
+    window.addEventListener("scroll", updateActiveNavLink);
+  
+    updateActiveNavLink();
+  });
